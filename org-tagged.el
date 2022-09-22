3d00a409485d4d7c6822996a538c08652ff6fd82
christian.koestlin <christian.koestlin@gmail.com>
2022-09-22 21:51:11 +0200

    style: Run package-lint against org-tagged.el

--- org-tagged.el
+++ org-tagged.el
@@ -1,4 +1,4 @@
-;;; org-tagged.el --- dynamic block for tagged org-mode todos. -*- lexical-binding: t -*-
+;;; org-tagged.el --- Dynamic block for tagged org-mode todos -*- lexical-binding: t -*-
 ;; Copyright (C) 2022 Christian Köstlin
 
 ;; This file is NOT part of GNU Emacs.
@@ -7,7 +7,7 @@
 ;; Keywords: org-mode, org, gtd, tools
 ;; Package-Requires: ((s) (dash "2.17.0") (emacs "24.4") (org "9.1"))
 ;; Package-Version: 0.0.1
-;; Homepage: http://gitlab.com/gizmomogwai/org-tagged
+;; Homepage: http://github.com/gizmomogwai/org-tagged
 
 ;;; Commentary:
 ;; To create a tagged table for an org file, simply put the dynamic block
@@ -23,7 +23,7 @@
 (require 'org)
 (require 'org-table)
 
-(defun org-tagged//map ()
+(defun org-tagged--map ()
   "Map one todoitem to the needed information.
 Return a list with
 - the heading
@@ -32,12 +32,12 @@ Return a list with
     (nth 4 (org-heading-components))
     (remove "" (s-split ":" (nth 5 (org-heading-components))))))
 
-(defun org-tagged//row-for (heading item-tags tags)
+(defun org-tagged--row-for (heading item-tags tags)
   "Create a row for a HEADING and its ITEM-TAGS for a table with TAGS."
   (format "|%s|" (s-join "|" (-map (lambda (tag)
                       (if (-elem-index tag item-tags) heading "")) tags))))
 
-(defun org-tagged/version ()
+(defun org-tagged-version ()
   "Print org-tagge version."
   (interactive)
   (message "org-tagged 0.0.1"))
@@ -51,11 +51,11 @@ PARAMS must contain: `:tags`."
       (
         (tags (s-split "|" (plist-get params :tags)))
         (table-title (plist-get params :tags))
-        (todos (org-map-entries 'org-tagged//map (plist-get params :match)))
-        (row-for (lambda (todo) (org-tagged//row-for (nth 0 todo) (nth 1 todo) tags)))
-        (table (s-join "\n" (-map row-for todos)))
-        )
+        (todos (org-map-entries 'org-tagged--map (plist-get params :match)))
+        (row-for (lambda (todo) (org-tagged--row-for (nth 0 todo) (nth 1 todo) tags)))
+        (table (s-join "\n" (-map row-for todos))))
       (format "|%s|\n|--|\n%s" table-title table)))
   (org-table-align))
 (provide 'org-tagged)
 ;;; org-tagged.el ends here
+
